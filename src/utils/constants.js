a2156d83c29a44d1df3ec8f6139e5700e867ade5
Viviorigi <duong682@gmail.com>
2024-03-15 20:38:18 +0700

    fix build_mode dev and production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,11 @@
-// export const API_ROOT ='http://localhost:8017'
-export const API_ROOT ='https://trello-api-kieq.onrender.com'
+
+let apiRoot = ''
+console.log('process.env', process.env)
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot='http://localhost:8017'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot='https://trello-api-kieq.onrender.com'
+}
+console.log('🚀 ~ apiRoot:', apiRoot)
+export const API_ROOT = apiRoot
